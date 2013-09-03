3af8def9644d0b555f3c588e666d8cb5f06c5164
usuario <d.lozano343@uniandes.edu.co>
2013-09-03 14:29:53 -0500

    factorial de un numero

---
+++ C/algebra/algebra.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <stdio.h>
+void main(int argc,char **argv){
+  int i=1;
+  int numero=atoi(argv[1]);
+  double factorial=1;
+  if(numero<=0){
+    printf("el numero debe ser mayor que cero")}
+ 
+ for(i=1;i<numero+1;i++){
+    factorial=factorial*i;
+  }
+ printf("el factorial del numero %d es %e \n",numero,factorial);
+}
