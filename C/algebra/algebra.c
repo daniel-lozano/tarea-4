b442cebacbc335a020e493f02f349365ebf250be
usuario <d.lozano343@uniandes.edu.co>
2013-09-05 13:37:19 -0500

    factorial

--- C/algebra/algebra.c
+++ C/algebra/algebra.c
@@ -5,7 +5,8 @@ void main(int argc,char **argv){
   int numero=atoi(argv[1]);
   double factorial=1;
   if(numero<=0){
-    printf("el numero debe ser mayor que cero")}
+    printf("el numero debe ser mayor que cero");
+    exit(1); }
  
  for(i=1;i<numero+1;i++){
     factorial=factorial*i;
