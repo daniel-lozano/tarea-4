4fe84d90eb3000ce01e0f68aa94fd46234d7a3bd
usuario <d.lozano343@uniandes.edu.co>
2013-09-17 15:03:43 -0500

    ejercicio 1 terminado, revisar errores

--- C/algebra/DanielLozano_polinomio.c
+++ C/algebra/DanielLozano_polinomio.c
@@ -9,6 +9,7 @@ void main(int argc,char **argv){
 
  //poner error de que flata el argumento para leer
 
+
   int Fl=atof(argv[2]);
   int numero= atoi(argv[2]);
 
@@ -41,31 +42,35 @@ void main(int argc,char **argv){
   rewind(file);
   printf("el numero de filas es %d \n", filas);
 
-
+  int i,j;
   // guarda los datos en las matrices
 
   x=malloc(sizeof(float)*filas);
 
   y=malloc(sizeof(float)*filas);
- 
+
+  printf("se guardan los datos\n"); 
+
   for(i=0;i<filas;i++){
-    fscanf(file,"%e %e", &x[i],&y[i]);
+    fscanf(file,"%lf %lf", &x[i],&y[i]);
     }
+ printf("se crearon las matrices de datos \n");
 
   // se crea el arreglo de datos de tamaño filas*(numero+1)
 
   int pos;
 
   //matriz
-  float *m1;
+  double *m1;
 
-  float *mt1;
+  double *mt1;
 
   int columnas=numero+1;
 
   m1=malloc(sizeof(float)*filas*(columnas));
   mt1=malloc(sizeof(float)*filas*(columnas));
 
+  printf("se le da memoria a m1 y mt1  \n");
   //crea m1
   for(i=0;i<filas;i++){
 
@@ -85,6 +90,8 @@ void main(int argc,char **argv){
       mt1[pos]=pow(x[i],j);
    }
   } 
+   
+   printf("m1 y mt1 creadas\n");
  
   //se crea la matriz en gsl}
   //el numero de columnas es igual al numero de entrada
@@ -92,8 +99,12 @@ void main(int argc,char **argv){
   gsl_matrix_view m= gsl_matrix_view_array (m1, filas, columnas);
   gsl_matrix_view mt= gsl_matrix_view_array (mt1, columnas, filas);
 
+  printf("m1 y mt1 siendo vistas por gsl") ;
+
   //vector d con entradas dadas por el arreglo Y
   gsl_matrix_view vect=gsl_matrix_view_array(y,filas,1);
+
+  printf("se creo vect\n");
 
   //necesario para la descomposicion LU
   int s;
@@ -102,62 +113,83 @@ void main(int argc,char **argv){
   gsl_matrix * Multi = gsl_matrix_alloc(columnas,columnas);
   gsl_matrix * prueva = gsl_matrix_alloc(columnas,columnas);
 
+  printf("se crearon las matrices multi y prueba\n");
+
   // matriz inversa
   gsl_matrix * Inver = gsl_matrix_alloc(columnas,columnas);
 
   //matriz total 
   gsl_matrix * Total = gsl_matrix_alloc(columnas,filas);
- 
-  //Multi y prueva son la multiplicacion de m y mt
 
-  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, mt, m, 0.0, Multi);
-  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, mt, m, 0.0, prueva);
+  printf("se crea la matriz total\n");
+
+  //Multi y prueva son la multiplicacion de m y mt
+  gsl_matrix * VECT;
+  VECT=&vect.matrix;
+  gsl_matrix * MT= gsl_matrix_alloc(columnas,filas);
+  MT=&mt.matrix;
+  gsl_matrix * M= gsl_matrix_alloc(filas,columnas);
+  M=&m.matrix;
+
+  printf("se le asignan las matrices \n");
+
+
+  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, MT, M, 0.0, Multi);
+  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, MT, M, 0.0, prueva);
 
   // matriz de permutacion necesaria para LU
-  gsl_permutation * perm = gsl_permutation_alloc (numero);
+  gsl_permutation * perm = gsl_permutation_alloc (columnas);
+  
+  printf("permutacion \n");
+  
   
   // se invierte la matriz de prueva que es 
-  gsl_linalg_LU_decomp( (prueva,perm,&s);
+  gsl_linalg_LU_decomp(prueva,perm,&s);
 		       
   //Invierte la matriz prueva y guarda el resultado en Inver
 
   gsl_linalg_LU_invert (prueva, perm, Inver);
+
+  printf("LU decomp\n");
 			
   //se guarda la matriz total en total
   //se tien(GtG)⁻1*Gt		      
 
-  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, Inver, mt, 0.0,Total);
+  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, Inver, MT, 0.0,Total);
    
   // se crea una matriz de resultado (m) y se hace el calculo---------------------------------------------
   gsl_matrix * Resultado = gsl_matrix_alloc(columnas,1);
 
-  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, Total, vect, 0.0, Resultado);
+  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, Total, VECT, 0.0, Resultado);
   	
   //se saca la matriz que tiene en sus posiciones F(Xi)---------------------------------------------------
 
   gsl_matrix * FuncionXi= gsl_matrix_alloc(filas,1);
 
-  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, m, resultado, 0.0, FuncionXi);
+  gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, M, Resultado, 0.0, FuncionXi);
 			
   // sacar shi
-  int i,j;			
-  int shi=0;			
+ 		
+  double shi=0;			
 			//gsl cuenta desde 1 o 0??
 
   for(i=0;i<filas;i++){
-
-    shi=shi+( y[i]- gsl_matrix_get (FuncionXi, i, 1))* (y[i]- gsl_matrix_get (FuncionXi, i, 1))/filas;
+    printf("%d\n",i);
+    printf("%lf\n",gsl_matrix_get(FuncionXi, i, 0));
+    shi=shi+ pow((y[i]- gsl_matrix_get(FuncionXi, i, 0)),2)/filas;
   }
 
 // imprime resultado 
 	     
    for(i=0;i<filas;i++){
 
-     printf("m%d = %e\n",i,gsl_matrix_get(Resultado,i,1));
+     printf("m%d = %lf\n",i,gsl_matrix_get(Resultado,i,0));
 
    }
 			
-   printf("X²=%e\n",shi);
+   printf("X²=%lf\n",shi);
 
   
 }
+
+
